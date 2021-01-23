c6b72c47fc0f4047270588e5ee94d91ff374d589
sharecodecdn <77809398+sharecodecdn@users.noreply.github.com>
2021-01-23 14:54:52 -0300

    Update videoContain.js

--- videoContain.js
+++ videoContain.js
@@ -1,5 +1,5 @@
 (function() {
     const videoContain = document.getElementById('videoContain')
     videoContain.style.width = '100%';
-    videoContain.style.height = videoContain.clientWidth / 1.5+'px'
+    videoContain.style.height = videoContain.clientWidth / 2+'px'
 })()
